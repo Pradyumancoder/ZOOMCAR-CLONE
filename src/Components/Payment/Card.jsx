5e0c40e2673f280a421e51843ad10767d40eac3a
pradyuman_shukla <shuklapradyuman786@gmail.com>
2022-08-27 23:22:27 +0530

    final submission of project

--- src/Components/Payment/Card.jsx
+++ src/Components/Payment/Card.jsx
@@ -1,20 +1,16 @@
 import { Alert, AlertIcon, Box, Button, Icon, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SimpleGrid, Stack, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import {CgCreditCard} from "react-icons/cg"
+import { Link } from 'react-router-dom'
 import { HomePage } from '../Homepage/HomePage'
 export const Card = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
-
-    // const handlepay=()=>{
-    //   <Stack spacing={3}>
-    //   <Alert status='success' variant='subtle'>
-    //     <AlertIcon />
-    //     Data uploaded to the server. Fire on!
-    //   </Alert>
-    //   </Stack>
-    //         }
-    //         handlepay()
-  return (
+ 
+  const handlepay=()=>{
+     alert("Vehicle now booked ✌️ ride safe")
+ }
+    
+  return(
    <SimpleGrid>
     <Box>
     <Box onClick={onOpen} borderBottom="1px"> <Icon as={CgCreditCard}/> Credit / Debit / ATM Card</Box>
@@ -36,16 +32,13 @@ export const Card = () => {
       <Button colorScheme='blue' mr={3} onClick={onClose}>
         Close
       </Button>
-      <Button   onClick={ <Stack spacing={3}>
-      <Alert status='success' variant='subtle'>
-        <AlertIcon />
-        Data uploaded to the server. Fire on!
-      </Alert>
-      </Stack>} variant='ghost' bg="yellow.300">Pay</Button>
+      <Link to ="/">
+      <Button onClick={handlepay}  variant='ghost' bg="yellow.300">Pay</Button>
+      </Link>
     </ModalFooter>
   </ModalContent>
 </Modal>
-{/* <HomePage/> */}
+
     </Box>
    </SimpleGrid>
   )
